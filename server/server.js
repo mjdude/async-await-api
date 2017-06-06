d2f871d3e10c21ed583b97726905f52e90cb3cf4
mjdude <mo@syncspot.net>
2017-06-06 17:25:52 -0400

    converting api to async await

--- server/server.js
+++ server/server.js
@@ -107,41 +107,41 @@ app.patch('/todos/:id', authenticate, (req, res) => {
 });
 
 // POST /users
-app.post('/users', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
-  var user = new User(body);
+app.post('/users', async (req, res) => {
 
-  user.save().then(() => {
-    return user.generateAuthToken();
-  }).then((token) => {
+  try {
+    const body = _.pick(req.body, ['email', 'password']);
+    const user = new User(body);
+    await user.save();
+    const token = await user.generateAuthToken();
     res.header('x-auth', token).send(user);
-  }).catch((e) => {
+  } catch(e) {
     res.status(400).send(e);
-  })
+  }
 });
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
-app.post('/users/login', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
-
-  User.findByCredentials(body.email, body.password).then((user) => {
-    return user.generateAuthToken().then((token) => {
+app.post('/users/login', async (req, res) => {
+  try {
+      const body = _.pick(req.body, ['email', 'password']);
+      const user = await User.findByCredentials(body.email, body.password);
+      const token = await user.generateAuthToken();
       res.header('x-auth', token).send(user);
-    });
-  }).catch((e) => {
-    res.status(400).send();
-  });
+  } catch (error) {
+      res.status(400).send();
+  }
 });
 
-app.delete('/users/me/token', authenticate, (req, res) => {
-  req.user.removeToken(req.token).then(() => {
+app.delete('/users/me/token', authenticate, async (req, res) => {
+  try {
+    await req.user.removeToken(req.token)     
     res.status(200).send();
-  }, () => {
+  } catch (error) {  
     res.status(400).send();
-  });
+  }  
 });
 
 app.listen(port, () => {
